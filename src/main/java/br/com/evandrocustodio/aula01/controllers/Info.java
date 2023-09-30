de1a1635428d11b29558c9424412395dddf29f8d
Evandro Custodio <evandrocustodio@gmail.com>
2023-09-30 01:00:37 -0300

    Ajustando projeto

--- src/main/java/br/com/evandrocustodio/aula01/controllers/Info.java
+++ src/main/java/br/com/evandrocustodio/aula01/controllers/Info.java
@@ -2,7 +2,6 @@ package br.com.evandrocustodio.aula01.controllers;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,34 +13,24 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.evandrocustodio.aula01.models.Despesa;
 import br.com.evandrocustodio.aula01.models.Informacoes;
-import br.com.evandrocustodio.aula01.services.ServiceDespesas;
 
 @Controller
-@RequestMapping(path = "/despesas")
+@RequestMapping(path = "/info")
 @CrossOrigin(origins = "*")
-public class ControlDespesas {
-
-	@Autowired
-	ServiceDespesas despesas;
+public class Info {
 
 	@Autowired
 	Environment environment;
-
+	
 	@Value("${aluno.nome}")
 	private String aluno;
-	
-	@GetMapping
-	public ResponseEntity<List<Despesa>> listaDesesa() {
-		return new ResponseEntity<List<Despesa>>(this.despesas.getDespesas(), HttpStatus.OK);
-	}
 
-	@GetMapping("/info")
+	@GetMapping
 	public ResponseEntity<Informacoes> getInfo() throws UnknownHostException {
 
 		String hostname = InetAddress.getLocalHost().getHostName();
-		Informacoes info = new Informacoes(hostname,aluno);
+		Informacoes info = new Informacoes(hostname, aluno);
 
 		return new ResponseEntity<Informacoes>(info, HttpStatus.OK);
 	}
---
+++ src/main/java/br/com/evandrocustodio/aula01/models/Informacoes.java
@@ -0,0 +1,12 @@
+package br.com.evandrocustodio.aula01.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Informacoes {
+
+	private String hostname;
+	private String aluno;
+}
